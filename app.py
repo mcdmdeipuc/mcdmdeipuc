07969bfbb9fa88bc06d18a17c423d1bb7553d5a6
mcdmdeipuc <153686133+mcdmdeipuc@users.noreply.github.com>
2023-12-13 21:30:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,6 @@ html_temp = """
 <div style ="background-color:white;padding:13px">
 <h1 style ="color:black;text-align:center;">PROJETO MCDM - PATENTE</h1></div>
 
-with st.container():
-  st.write("---")
-
-  st.subheader("Informações gerais")
-
-# display the front end aspect
-st.markdown(html_temp, unsafe_allow_html = True)
-st.caption('Mestrado acadêmico em Engenharia de Produção - PUC - Rio')
-st.caption('DEI - Departamento de Engenharia Industrial')
-st.caption('by Jackeline Alves, Rodrigo Caiado, Renan Santos')
-st.caption('Em 2023')
 
 
 # display the front end aspect
