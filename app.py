5bedd341c08eeefbfe64fbaa61bd7714f221f596
mcdmdeipuc <153686133+mcdmdeipuc@users.noreply.github.com>
2024-02-09 17:29:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ else:
         
     #07
     st.subheader("1.1 - Gerando a Matriz de comparação dos 5 critérios - Decisor Gerente:")
-    def tratando_erro(): 
+def tratando_erro(): 
     try: 
         sheetNr = 0
             print(desafioLabels[sheetNr])
