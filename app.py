8850d9a111a7a5c23d8097acd0e9c7ecbfbac25f
mcdmdeipuc <153686133+mcdmdeipuc@users.noreply.github.com>
2024-02-10 10:08:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ backgroundColor = "#ADD8E6"
 # set page configuration
 st.set_page_config(
 page_title= "MESTRADO",
-page_icon="🧊",
-layout="wide",
-initial_sidebar_state="auto",  # Pode ser "auto", "expanded" ou "collapsed"
+#layout="wide",
+#initial_sidebar_state="auto",  # Pode ser "auto", "expanded" ou "collapsed"
 )
 
 html_temp = """
