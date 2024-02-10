6838070553ee9fee5a8ac8a40415bda9213af4bd
mcdmdeipuc <153686133+mcdmdeipuc@users.noreply.github.com>
2024-02-09 22:14:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ with st.container():
     st.markdown("<h2 style='text-align: center;'>01 - Método AHP</h2>", unsafe_allow_html=True)
 #07
 st.subheader("1.1 - Gerando a Matriz de comparação dos 5 critérios - Decisor Gerente:")
-try:
+    try:
          sheetNr = 0
          print(desafioLabels[sheetNr])
                   
